334916f5c333bd56fad08e208493ad0fbddf19fc
Carl <carlhuang@sunnygo.com.tw>
2023-11-27 13:09:39 +0800

    Modiyf

--- app/(root)/pair/_components/Main/CardContainer.tsx
+++ app/(root)/pair/_components/Main/CardContainer.tsx
@@ -1,10 +1,9 @@
-'use client'
-
 import { cn } from '@/lib/utils'
+import React from 'react'
 
 function CardContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative flex h-[500px] items-center justify-center   ">
+    <div className="relative mx-auto flex h-[500px] w-[500px] items-center justify-center overflow-hidden ">
       <div
         className={cn(
           'absolute -z-10 h-[444.23px] w-[276.79px]   rounded-[32px] bg-white shadow-[0px_9px_11px_0px_#00000040]',
@@ -17,13 +16,7 @@ function CardContainer({ children }: { children: React.ReactNode }) {
           'translate-x-4 translate-y-[3vw] rotate-[14deg] scale-[0.85]',
         )}
       />
-      <div
-        className={cn(
-          'box-content  flex h-[444.23px]  w-[276.79px] rotate-[-3deg]  items-center justify-center overflow-hidden  rounded-[32px] border-[0.9375vw] border-white bg-gradient-to-b from-slate-100 to-black shadow-[-6px_9px_11px_0px_#00000040]',
-        )}
-      >
-        {children}
-      </div>
+      {children}
     </div>
   )
 }
--- app/(root)/pair/_components/Main/DropCardArea.tsx
+++ app/(root)/pair/_components/Main/DropCardArea.tsx
@@ -1,89 +1,85 @@
+import CardContainer from './CardContainer'
 import { FillImage } from '@/components/fill-image'
 import { cn } from '@/lib/utils'
-import {
-  motion,
-  useMotionValue,
-  useTransform,
-  DragHandlers,
-} from 'framer-motion'
+import { motion, useMotionValue, useTransform } from 'framer-motion'
+import { X } from 'lucide-react'
 import React from 'react'
 
-const Test = () => {
+function DropCardArea() {
   const x = useMotionValue(0)
   const y = useMotionValue(0)
   const rotate = useTransform(x, [-100, 0, 100], [-15, -3, 15])
   const opacity = useTransform([x, y], ([xValue, yValue]: number[]) => {
     const totalDistance = Math.sqrt(xValue ** 2 + yValue ** 2)
-    const opacityValue = 1 - totalDistance / 100
-    return opacityValue < 0.5 ? 0.5 : opacityValue
+    const opacityValue = 1 - totalDistance / 1000
+    return opacityValue < 0.8 ? 0.8 : opacityValue
   })
 
-  const onDragEnd = () => {}
+  const dragCardClassName =
+    'box-content flex h-[444.23px]  w-[276.79px]   cursor-pointer items-center justify-center  overflow-hidden rounded-[32px] border-[0.9375vw] border-white bg-gradient-to-b from-slate-100 to-black shadow-[-6px_9px_11px_0px_#00000040]'
 
+  const handleDisLike = () => {
+    x.set(-300)
+  }
+  const handleLike = () => {
+    console.log('like')
+    x.set(300)
+  }
   return (
-    <motion.div
-      className={cn(
-        'box-content flex h-[444.23px]  w-[276.79px]   cursor-pointer items-center justify-center  overflow-hidden rounded-[32px] border-[0.9375vw] border-white bg-gradient-to-b from-slate-100 to-black shadow-[-6px_9px_11px_0px_#00000040]',
-      )}
-      drag
-      dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-      onDragEnd={(event, info) => {
-        x.set(0)
-        y.set(0)
-      }}
-      style={{ x, y, rotate }}
-      onDrag={(event, info) => {
-        console.log('拖曳位置x:', info.point.x)
-        console.log('拖曳位置y:', info.point.y)
-      }}
-    >
-      <FillImage
-        className=" pointer-events-none"
-        src="/images/main/dogImg.png"
-        alt=""
-      />
-    </motion.div>
-  )
-}
-
-function DropCardArea() {
-  return (
-    <div className="relative flex h-[500px] items-center justify-center overflow-hidden  ">
-      <div
-        className={cn(
-          'absolute -z-10 h-[444.23px] w-[276.79px]   rounded-[32px] bg-white shadow-[0px_9px_11px_0px_#00000040]',
-          'translate-y-[-2vw] rotate-[14deg] scale-[0.92]',
-        )}
-      />
-      <div
-        className={cn(
-          'absolute -z-10 h-[444.23px] w-[276.79px]  rounded-[32px] bg-white shadow-[0px_9px_11px_0px_#00000040]',
-          'translate-x-4 translate-y-[3vw] rotate-[14deg] scale-[0.85]',
-        )}
-      />
-      <div
-        className={cn(
-          'absolute box-content  flex h-[444.23px]  w-[276.79px] rotate-[-3deg]  items-center justify-center overflow-hidden  rounded-[32px] border-[0.9375vw] border-white bg-gradient-to-b from-slate-100 to-black shadow-[-6px_9px_11px_0px_#00000040]',
-        )}
-      >
+    <CardContainer>
+      <div className={cn(dragCardClassName, 'absolute rotate-[-3deg] ')}>
         <FillImage
           className=" pointer-events-none"
           src="/images/main/dogImg.png"
           alt=""
         />
       </div>
-      <Test />
-      <div className="absolute bottom-12 flex h-[56px] w-[255px] items-center justify-between">
-        <div>x</div>
-        <div>
-          <div>
-            123123 <span>男</span>
-          </div>
-          <div>我是地址</div>
+      <motion.div
+        className={dragCardClassName}
+        drag
+        dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+        onDragEnd={(event, info) => {
+          x.set(0)
+          y.set(0)
+        }}
+        style={{ x, y, rotate, opacity }}
+        onDrag={(event, info) => {
+          console.log('拖曳位置x:', info.point.x)
+          console.log('拖曳位置y:', info.point.y)
+        }}
+      >
+        <FillImage
+          className=" pointer-events-none"
+          src="/images/main/dogImg.png"
+          alt=""
+        />
+      </motion.div>
+      <div className="absolute bottom-12 left-[37%]  flex h-[54px] w-[245px] items-center justify-between">
+        <div
+          onClick={handleDisLike}
+          className="flex h-[50px] w-[50px] cursor-pointer items-center justify-center rounded-full bg-white"
+        >
+          <X />
         </div>
-        <div>愛心</div>
+        <div className="min-w-[94px] text-white">
+          <div className="flex items-center justify-between text-lg ">
+            123123
+            <span className="h-[24px] w-[24px]">
+              <FillImage src="/images/icons/female.png" />
+            </span>
+          </div>
+          <div className="text-sm">我是地址</div>
+        </div>
+        <div
+          onClick={handleLike}
+          className="flex h-[50px] w-[50px] cursor-pointer items-center justify-center rounded-full bg-white"
+        >
+          <div className="h-[31px] w-[31px]">
+            <FillImage src="/images/icons/love_S.png" />
+          </div>
+        </div>
       </div>
-    </div>
+    </CardContainer>
   )
 }
 
